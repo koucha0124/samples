04cbe8a8593598049bc694f26b1def9a4e66ea4f
Koucha0124 <84267040+Koucha0124@users.noreply.github.com>
2021-07-15 17:51:58 +0900

    Create main.py

---
+++ Part5/main.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+from discord_buttons_plugin import *
+
+bot = commands.Bot(command_prefix = "m!")
+buttons = ButtonsClient(bot)
+
+@bot.event
+async def on_ready():
+	print("準備完了")
+  
+@buttons.click
+async def button_hello(ctx):
+	await ctx.reply("こんにちは！")
+
+@buttons.click
+async def button_ephemeral(ctx):
+	await ctx.reply("このメッセージはあなたにしか見えていません！", flags = MessageFlags().EPHEMERAL)
+
+@bot.command()
+async def create(ctx):
+	await buttons.send(
+		content = "テストボタン", 
+		channel = ctx.channel.id,
+		components = [
+			ActionRow([
+				Button(
+					label="Hello", 
+					style=ButtonType().Primary, 
+					custom_id="button_hello"
+				)
+			]),ActionRow([
+				Button(
+					label="Ephemeral",
+					style=ButtonType().Danger,
+					custom_id="button_ephemeral"
+				)
+			])
+		]
+	)
+
+bot.run(Token)
