1780b7ec4a9a6c922fd43aed319f77d1f8d22d35
Koucha0124 <84267040+Koucha0124@users.noreply.github.com>
2021-07-29 17:10:41 +0900

    Update main.py

--- Part7/main.py
+++ Part7/main.py
@@ -1,1 +1,17 @@
+import discord
+from discord.ext import commands
+#pip install requests
+import datetime
 
+
+def jst():
+    now = datetime.datetime.utcnow()
+    now = now + datetime.timedelta(hours=9)
+    return now
+
+@bot.event
+async def on_ready():
+	print("準備完了")
+    
+
+bot.run(Token)
