fe9bce7c338ccb920ef45a42702a79bc33437d81
こうちゃ <84267040+koucha0124@users.noreply.github.com>
2024-02-21 15:47:19 +0900

    Update main.py
    2024/2/21 不具合のため修正済み

--- Part5/main.py
+++ Part5/main.py
@@ -4,7 +4,8 @@ from discord.ext import commands
 from discord_buttons_plugin import *
 #pip install requests
 import requests
-bot = commands.Bot(command_prefix = "m!")
+
+bot = commands.Bot(command_prefix = "m!", intents=discord.Intents.all())
 buttons = ButtonsClient(bot)
 
 @bot.event
